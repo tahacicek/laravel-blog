f85160e3408e6470dabc32e0876d2f88b6eae43d
Taha Çiçek <ttahacicek@gmail.com>
2022-08-29 21:45:46 +0300

    mail completed.

--- app/Http/Controllers/Customer/Homepage.php
+++ app/Http/Controllers/Customer/Homepage.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Customer;
+
 use Illuminate\Support\Facades\File;
 use App\Http\Requests\PostRequest;
 use Illuminate\Database\Eloquent\Model;
@@ -12,6 +13,8 @@ use Dotenv\Store\File\Paths;
 use Illuminate\Support\Facades\Storage;
 use SebastianBergmann\CodeCoverage\NoCodeCoverageDriverWithPathCoverageSupportAvailableException;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+
 
 //models
 use App\Models\Page;
@@ -26,10 +29,9 @@ use function GuzzleHttp\Promise\all;
 class Homepage extends Controller
 {
     public function __construct()
-    {   
+    {
         view()->share("pages", Page::orderBy("order", "ASC")->get());
         view()->share("categories", Category::inRandomOrder()->get());
-
     }
     public function index()
     {
@@ -46,53 +48,62 @@ class Homepage extends Controller
         return view("customer.blog-detail", $data);
     }
 
-    public function category($slug){
-
+    public function category($slug)
+    {
         $category = Category::whereSlug($slug)->first() ?? abort(403, "Böyle bir kategori bulunumadı");
-        $data["post"]=Post::where("category_id", $category->id)->inRandomOrder()->paginate(2);
-        $data["category"]=$category;
+        $data["post"] = Post::where("category_id", $category->id)->inRandomOrder()->paginate(2);
+        $data["category"] = $category;
         return view("customer.category", $data);
-
     }
 
-    public function page($slug){
-        $page=Page::whereSlug($slug)->first() ?? abort(403, "Böyle bir kategori bulunumadı");
-        $data["page"]=$page;
-        return view("customer.page", $data );
-
+    public function page($slug)
+    {
+        $page = Page::whereSlug($slug)->first() ?? abort(403, "Böyle bir kategori bulunumadı");
+        $data["page"] = $page;
+        return view("customer.page", $data);
     }
 
-    public function contact(){
+    public function contact()
+    {
         return view("customer.contact");
     }
-   
-    public function contact_post(Request $request){
-      
+
+    public function contact_post(Request $request)
+    {
         $rules = [
-            "name"=>"required|min:5"
-          
+            "email" => "required|email",
+            "topic" => "required",
+            "message" => "required|min:10"
+
         ];
 
-        $validated=Validator::make($request->all(), $rules);
-        
+        $validated = Validator::make($request->all(), $rules);
         if ($validated->fails()) {
             toastr()->error("İsim alanı boş bırakılamaz ve en az beş karakter olmalıdır.", "Dikkat!");
             return redirect()->route("contact");
-           
         }
-           
-        
+
+        Mail::send([], [], function($message) use($request){
+                  $message->from("iletisim@blogsites.com", "Easoft");
+                  $message->to("ttahacicek@gmail.com");
+                  $message->setBody(
+                  "Mesajı gönderen : $request->name </br>
+                  Mesajı gönderen mail :  '.$request->email </br>
+                  Mesajı konusu :  $request->topic </br>
+                  Mesajı : $request->message </br>
+                  Mesaj gönderilme tarihi : .'.now().'., 'text/html'");
+                  $message->subject($request->name. "İletişimden mesaj gönderdi!");
+        });
+
         $contact = new Contact;
-        $contact->name=$request->name;
-        $contact->email=$request->email;
-        $contact->topic=$request->topic;
-        $contact->message=$request->message;
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->topic = $request->topic;
+        $contact->message = $request->message;
         $contact->save();
 
-        
         toastr()->success("Başarılı", 'Mesajınız başarıyla gönderildi, en kısa zamanda dönüş sağlanacaktır.');
 
         return redirect()->route("contact");
-
     }
 }
